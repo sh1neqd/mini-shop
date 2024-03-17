bcc484b1416d923f6fc821749ea28872ef13f143
sh1neqd <barton.g.bogdan@gmail.com>
2024-03-17 22:24:24 +0500

    edited db migrations

--- app/internal/domain/item/model.go
+++ app/internal/domain/item/model.go
@@ -1,7 +1,6 @@
 package item
 
 type Item struct {
-	ID         int32  `json:"id"`
-	Name       string `json:"name"`
-	CategoryId int32  `json:"category_id"`
+	ID   int32  `json:"id"`
+	Name string `json:"name"`
 }
